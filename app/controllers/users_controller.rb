0b3ad5a1886d8542bdd594dddad05a0abee2cf7b
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-04-06 20:02:26 -0600

    Add login action and update create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save && params[:password] == params[:confirm_password]
-      User.create(user_params)
+    @user = User.create(user_params)
+    if @user.save && user_params[:password] == user_params[:confirm_password]
       flash[:notice] = "Welcome #{@user.name}! You are now registered and logged in!"
       redirect_to '/profile'
-    elsif params[:password] != params[:confirm_password]
+    elsif user_params[:password] != user_params[:confirm_password]
       flash[:notice] = "Your password fields do not match. #{@user.errors.full_messages.to_sentence}"
       render(:new)
     else
       flash[:notice] = @user.errors.full_messages.to_sentence
       render(:new)
     end
+  end
+
+  def login
   end
 
   def profile
@@ -26,7 +28,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :street_address, :city, :state, :zip_code, :email_address, :password)
+    params.require(:user).permit(:name, :street_address, :city, :state, :zip_code, :email_address, :password, :confirm_password)
   end
 
 end
