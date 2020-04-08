2c8c6a8aecdd2ea551bb59c265f2c55425323d3d
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-07 20:55:03 -0600

    Add namespace admin for dashboard and user names

--- config/routes.rb
+++ config/routes.rb
@@ -38,9 +38,10 @@ Rails.application.routes.draw do
   get '/register', to: "users#new"
   post '/register', to: "users#create"
 
+  get '/logout', to: 'users#logout'
+
   namespace :user do
     get '/profile', to: "users#profile"
-    get '/logout', to: 'users#logout'
     post '/profile', to: 'sessions#create'
   end
 
@@ -51,6 +52,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
 
   namespace :admin do
-    get '/dashboard', to: 'dashboard#index'
+    get '/', to: 'dashboard#index'
+    get '/users', to: 'users#user_names'
   end
 end
