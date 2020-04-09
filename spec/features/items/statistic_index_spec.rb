a6b22ff8aea5b7f4864cbf8a4be896c35be39644
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-04-09 13:55:51 -0600

    Update names of items in test

--- spec/features/items/statistic_index_spec.rb
+++ spec/features/items/statistic_index_spec.rb
@@ -18,13 +18,13 @@ RSpec.describe 'As a visitor' do
     @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
     @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
     @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
-    @pens = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 5, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 10)
-    @gloves = @meg.items.create(name: "Yellow Pencil", description: "It's a glove!!", price: 25, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 40)
-    @chains = @meg.items.create(name: "Yellow Pencil", description: "It's a chain!", price: 9, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 23)
-    @leggings = @meg.items.create(name: "Yellow Pencil", description: "It's a legging!", price: 30, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 59)
-    @index_cards = @mike.items.create(name: "Yellow Pencil", description: "It's an index card!", price: 83, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 9)
-    @folders = @mike.items.create(name: "Yellow Pencil", description: "It's a folder!", price: 8, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 19)
-    @helmet = @meg.items.create(name: "Yellow Pencil", description: "It's a helmet!", price: 12, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 62)
+    @pens = @mike.items.create(name: "Pens", description: "You can write on paper with it!", price: 5, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 10)
+    @gloves = @meg.items.create(name: "Gloves", description: "It's a glove!!", price: 25, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 40)
+    @chains = @meg.items.create(name: "Chains", description: "It's a chain!", price: 9, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 23)
+    @leggings = @meg.items.create(name: "Leggings", description: "It's a legging!", price: 30, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 59)
+    @index_cards = @mike.items.create(name: "Index Cars", description: "It's an index card!", price: 83, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 9)
+    @folders = @mike.items.create(name: "Folders", description: "It's a folder!", price: 8, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 19)
+    @helmet = @meg.items.create(name: "Helment", description: "It's a helmet!", price: 12, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 62)
 
     visit '/'
 
@@ -160,24 +160,16 @@ RSpec.describe 'As a visitor' do
     expect(page).to have_content("Most Popular")
     expect(page).to have_content("Least Popular")
 
-    within 'most-popular' do
+    within '.least-popular' do
       expect(page).to have_content(@leggings.name)
       expect(page).to have_content(@pens.name)
       expect(page).to have_content(@paper.name)
     end
 
-    within 'least-popular' do
+    within '.most-popular' do
       expect(page).to have_content(@index_cards.name)
       expect(page).to have_content(@gloves.name)
       expect(page).to have_content(@helmet.name)
     end
   end
 end
-
-
-# As any kind of user on the system
-# When I visit the items index page ("/items")
-# I see an area with statistics:
-# the top 5 most popular items by quantity purchased, plus the quantity bought
-# the bottom 5 least popular items, plus the quantity bought
-# "Popularity" is determined by total quantity of that item ordered
