668234ec3a2cc5ff69aed00d10a617b812c22f1f
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-04-08 21:31:48 -0600

    Feature test for editing user info

---
+++ spec/features/users/edit_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "As a registered user" do
+  it "can see all profile data except password on the profile page with link to edit profile data" do
+    user = User.create({name: "Bob", street_address: "22 dog st", city: "Fort Collins",
+                         state: "CO", zip_code: "80375", email_address: "bob@example.com",
+                         password: "password1", password_confirmation: "password1", role: 0
+                        })
+    visit '/'
+
+    click_link 'Log in'
+
+    fill_in :email_address, with: 'bob@example.com'
+    fill_in :password, with: 'password1'
+    click_button 'Log in'
+    click_link("Edit Profile Info")
+
+    expect(current_path).to eq('/user/profile/edit')
+    fill_in :street_address, with: "531 Pearl"
+    fill_in :city, with: "Denver"
+    # fill_in :zip_code, with: "80210"
+    click_on('Submit')
+
+    expect(current_path).to eq('/user/profile')
+    expect(page).to have_content('Your information is updated')
+
+    user.reload
+
+    expect(current_path).to eq('/user/profile')
+    expect(page).to have_content("Logged in as #{user.name}")
+    expect(page).to have_content(user.name)
+    expect(page).to have_content(user.street_address)
+    expect(page).to have_content(user.city)
+    expect(page).to have_content(user.state)
+    expect(page).to have_content(user.zip_code)
+    expect(page).to have_content(user.email_address)
+  end
+end
