5b063be02dd314dddf26b664dee7a9f08231fcda
Margo Flewelling <flewelling.margo@gmail.com>
2020-04-09 20:19:01 -0600

    Add to merchant dashboard view

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Merchant::DashboardController < Merchant::BaseController
   def index
     @merchant = Merchant.find(current_user.merchant_id)
+    require "pry"; binding.pry
+    Order.joins(:item).where(item: {merchant: @merchant})
   end
 end
