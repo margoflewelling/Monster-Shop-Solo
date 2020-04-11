b2230268cda93638593bedaf1aab6513dea3db84
Margo Flewelling <flewelling.margo@gmail.com>
2020-04-11 09:11:36 -0600

    Add partial for the merchant items index

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -2,6 +2,7 @@ class Merchant::ItemsController < Merchant::BaseController
 
   def index
     @merchant = Merchant.find(current_user.merchant_id)
+    @items = @merchant.items
   end
-  
+
 end
--- spec/features/items/merchant_items_index_spec.rb
+++ spec/features/items/merchant_items_index_spec.rb
@@ -60,5 +60,44 @@ RSpec.describe "Merchant Items Index Page" do
         expect(page).to have_content("Inventory: #{@shifter.inventory}")
       end
     end
+
+    it "can deactivate an item from their items page" do
+      visit "/merchant"
+      click_link 'View My Items'
+      expect(current_path).to eq("/merchant/items")
+      save_and_open_page
+      within "#merch-item-#{@tire.id}" do
+        expect(page).to have_content(@tire.name)
+        expect(page).to have_content("Price: $#{@tire.price}")
+        expect(page).to have_css("img[src*='#{@tire.image}']")
+        expect(page).to have_content("Active")
+        expect(page).to_not have_content(@tire.description)
+        expect(page).to have_content("Inventory: #{@tire.inventory}")
+        click_link("Deactivate")
+        expect(current_path).to eq("/merchant/items")
+        expect(page).to have_content("#{@tire.name} is no longer for sale")
+        expect(page).to have_content("Inactive")
+      end
+    end
+
   end
 end
+
+
+
+# User Story 42, Merchant deactivates an item
+#
+# As a merchant employee
+# When I visit my items page
+# I see all of my items with the following info:
+#  - name
+# - description
+# - price
+# - image
+# - active/inactive status
+# - inventory
+# I see a link or button to deactivate the item next to each item that is active
+# And I click on the "deactivate" button or link for an item
+# I am returned to my items page
+# I see a flash message indicating this item is no longer for sale
+# I see the item is now inactive
