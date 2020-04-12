4f7e6ab0c22c966e639c6c404cd7f63070bd3f48
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-12 17:14:26 -0600

    Added index to the merchant controller and a view

--- app/controllers/admin/merchant_controller.rb
+++ app/controllers/admin/merchant_controller.rb
@@ -1,5 +1,9 @@
 class Admin::MerchantController < Admin::BaseController
 
+  def index
+    @merchants = Merchant.all
+  end
+
   def show
     @merchant = Merchant.find(params[:merchant_id])
   end
