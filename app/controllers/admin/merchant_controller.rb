b479349acc3655be1fcab593c7141aef64c145a9
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-12 19:34:02 -0600

    Add logic to disable a merchant

--- app/controllers/admin/merchant_controller.rb
+++ app/controllers/admin/merchant_controller.rb
@@ -7,5 +7,17 @@ class Admin::MerchantController < Admin::BaseController
   def show
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def disable
+    @merchants = Merchant.all
+    @merchants.each do |merchant|
+      if merchant.active?
+        flash[:notice] = "#{merchant.name}'s account is now disabled"
+        merchant.update(active?: false)
+        merchant.deactivate_items
+      end
+    end
+    redirect_to '/admin/merchants'
+  end
   
 end
