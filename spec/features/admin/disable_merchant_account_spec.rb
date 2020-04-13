9197abbe500550f44a9e625afb070b8442e686d3
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-12 19:32:54 -0600

    Updated test to include withins

--- spec/features/admin/disable_merchant_account_spec.rb
+++ spec/features/admin/disable_merchant_account_spec.rb
@@ -3,6 +3,16 @@ require 'rails_helper'
 RSpec.describe 'As an admin' do
   describe 'When I visit the admin merchant page' do
     it 'I see a disable button next to all active merchants' do
+      regina = User.create({name: "Regina",
+                             street_address: "6667 Evil Ln",
+                             city: "Storybrooke",
+                             state: "ME",
+                             zip_code: "00435",
+                             email_address: "evilqueen@example.com",
+                             password: "henry2004",
+                             password_confirmation: "henry2004",
+                             role: 2,
+                            })
       meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       brian = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203, active?:false)
 
@@ -10,19 +20,28 @@ RSpec.describe 'As an admin' do
       pull_toy = meg.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
       dog_bone = meg.items.create(name: "Dog Bone", description: "They'll love it!", price: 20, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
 
-      visit '/admin/merchants'
+      visit '/'
 
-      within "#cart-item-#{tire.id}" do
+      click_on 'Log in'
+      fill_in :email_address, with: 'evilqueen@example.com'
+      fill_in :password, with: 'henry2004'
+      click_button 'Log in'
+
+      visit "/merchants/#{meg.id}/items"
+
+      within "#item-#{tire.id}" do
         expect(tire.active?).to be_truthy
       end
 
-      within "#cart-item-#{pull_toy.id}" do
+      within "#item-#{pull_toy.id}" do
         expect(pull_toy.active?).to be_truthy
       end
 
-      within "#cart-item-#{dog_bone.id}" do
+      within "#item-#{dog_bone.id}" do
         expect(dog_bone.active?).to be_falsey
       end
+
+      visit '/admin/merchants'
 
       within "#merch-shop-#{brian.id}" do
         expect(page).to have_no_content('disable')
@@ -35,28 +54,24 @@ RSpec.describe 'As an admin' do
       expect(current_path).to eq('/admin/merchants')
       expect(page).to have_content("#{meg.name}'s account is now disabled")
 
-      within "#cart-item-#{tire.id}" do
-        expect(tire.active?).to be_falsey
-      end
-
-      within "#cart-item-#{pull_toy.id}" do
-        expect(pull_toy.active?).to be_falsey
-      end
-
-      within "#cart-item-#{dog_bone.id}" do
-        expect(dog_bone.active?).to be_falsey
-      end
-
       within "#merch-shop-#{meg.id}" do
         expect(page).to have_no_content('disable')
       end
+
+      visit "/merchants/#{meg.id}/items"
+
+      within "#item-#{tire.id}" do
+        expect(page).to have_content('Inactive')
+      end
+
+      within "#item-#{pull_toy.id}" do
+        expect(page).to have_content('Inactive')
+      end
+
+      within "#item-#{dog_bone.id}" do
+        expect(page).to have_content('Inactive')
+      end
+
     end
   end
 end
-
-# As an admin
-# When I visit the admin's merchant index page ('/admin/merchants')
-# I see a "disable" button next to any merchants who are not yet disabled
-# When I click on the "disable" button
-# I am returned to the admin's merchant index page where I see that the merchant's account is now disabled
-# And I see a flash message that the merchant's account is now disabled
