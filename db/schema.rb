545021fecc358583c900c7588f7050c8e46d694c
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-13 16:32:58 -0600

    Migrated Migrations from Daniels stories

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20200412203959) do
-=======
 ActiveRecord::Schema.define(version: 20200412224247) do
->>>>>>> 2371832aeb6aa0e2ceee74d6421b34256bfa69e4
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
