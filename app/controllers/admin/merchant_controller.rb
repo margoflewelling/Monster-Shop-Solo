00cc5e60e8a56101b40996bf42f7fe503b87e018
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-12 21:22:20 -0600

    Add logic to the merchant controller for admin enable

--- app/controllers/admin/merchant_controller.rb
+++ app/controllers/admin/merchant_controller.rb
@@ -19,5 +19,16 @@ class Admin::MerchantController < Admin::BaseController
     end
     redirect_to '/admin/merchants'
   end
-  
+
+  def enable
+    @merchants = Merchant.all
+    @merchants.each do |merchant|
+      if !merchant.active?
+        flash[:notice] = "#{merchant.name}'s account is now enabled"
+        merchant.update(active?: true)
+        merchant.activate_items
+      end
+    end
+    redirect_to '/admin/merchants'
+  end
 end
