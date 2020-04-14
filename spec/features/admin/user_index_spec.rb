a132906c52922fa8cc3a31f756aea88f913c530b
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-04-14 15:15:30 -0600

    Delete user story comments

--- spec/features/admin/user_index_spec.rb
+++ spec/features/admin/user_index_spec.rb
@@ -92,14 +92,3 @@ RSpec.describe 'As an admin user I can visit the user index page' do
     end
   end
 end
-
-# User Story 53, Admin User Index Page
-#
-# As an admin user
-# When I click the "Users" link in the nav (only visible to admins)
-# Then my current URI route is "/admin/users"
-# Only admin users can reach this path.
-# I see all users in the system
-# Each user's name is a link to a show page for that user ("/admin/users/5")
-# Next to each user's name is the date they registered
-# Next to each user's name I see what type of user they are
