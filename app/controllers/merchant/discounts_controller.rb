b0575944edee1c439cdc21f4c695833ad6ea10f3
Margo Flewelling <flewelling.margo@gmail.com>
2020-04-18 14:26:43 -0600

    Add methods for destroying and creating a discount

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,6 +1,26 @@
 class Merchant::DiscountsController< Merchant::BaseController
 
   def index
+    @merchant = current_user
+  end
+
+  def create
+    @merchant = current_user
+    @merchant.merchant.discounts.create(discount_params)
+    redirect_to "/merchant/discounts"
+  end
+
+  def destroy
+    discount = Discount.find(params[:id])
+    discount.destroy
+    redirect_to "/merchant/discounts"
+  end
+
+
+private
+
+  def discount_params
+    params.permit(:percentage, :min_quantity)
   end
 
 end
