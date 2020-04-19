5d49b90c3923941a8c8d9eddaadbb049b8b938a1
Margo Flewelling <flewelling.margo@gmail.com>
2020-04-19 09:20:21 -0600

    Write tests for ensuring discounts apply in the cart to the subtotal and total

--- spec/features/discounts/applying_discount_spec.rb
+++ spec/features/discounts/applying_discount_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "applying discount to the cart", type: :feature do
     before(:each) do
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @bill = Merchant.create(name: "Bill's Dog Shop", address: '123 Dog Rd.', city: 'Denver', state: 'CO', zip: 80203)
 
       @user = User.create({name: "Regina",
                            street_address: "6667 Evil Ln",
@@ -18,10 +19,11 @@ RSpec.describe "applying discount to the cart", type: :feature do
 
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      @bone = @bill.items.create(name: "Bone", description: "Your dog will love it", price: 10, image: "https://assets.petco.com/petco/image/upload/f_auto,q_auto,t_ProductDetail-large/2730736-center-1", inventory: 20)
 
       @discount_1 = @meg.discounts.create(percentage: 20, min_quantity: 3)
-      @discount_2 = @meg.discounts.create(percentage: 30, min_quantity: 5)
+      @discount_2 = @meg.discounts.create(percentage: 50, min_quantity: 5)
 
       visit '/'
       click_link 'Log in'
@@ -30,7 +32,7 @@ RSpec.describe "applying discount to the cart", type: :feature do
       click_button 'Log in'
     end
 
-    it "can see that a discount has applied to the cart" do
+    it "can see that a discount has applied to the cart and only applies to qualifying items" do
       click_on "All Items"
       click_on "Gatorskins"
       click_on "Add To Cart"
@@ -47,6 +49,47 @@ RSpec.describe "applying discount to the cart", type: :feature do
       expect(page).to_not have_content("Total: $450")
     end
 
+    it "can only use discount on items from that merchant with the discount" do
+      click_on "All Items"
+      click_on "Gatorskins"
+      click_on "Add To Cart"
+      click_on "Chain"
+      click_on "Add To Cart"
+      click_on "Bone"
+      click_on "Add To Cart"
+      click_on "Cart"
+
+      within "#cart-item-#{@tire.id}" do
+        click_on "+"
+        click_on "+"
+        click_on "+"
+      end
+      within "#cart-item-#{@bone.id}" do
+        click_on "+"
+        click_on "+"
+        click_on "+"
+      end
+      page.refresh
+      expect(page).to have_content("Total: $410")
+    end
+
+    it "can choose the better discount if two are available" do
+      click_on "All Items"
+      click_on "Gatorskins"
+      click_on "Add To Cart"
+      click_on "Chain"
+      click_on "Add To Cart"
+      click_on "Cart"
+      within "#cart-item-#{@tire.id}" do
+        click_on "+"
+        click_on "+"
+        click_on "+"
+        click_on "+"
+      end
+      save_and_open_page
+      page.refresh
+      expect(page).to have_content("Total: $300")
+    end
   end
 
 
